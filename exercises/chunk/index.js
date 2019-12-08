3da15c0c38aec725322cd7de2152356cb5002c00
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2019-12-08 01:27:35 +0100

    Update chunk

---
+++ exercises/chunk/index.js
@@ -0,0 +1,23 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+    const prime = array.slice(0, size);
+    const fEnd = array.slice(size);
+
+    if(!array.length) {
+        return []
+    }
+
+    return [prime, ...chunk(fEnd, size)]
+}
+console.log(chunk([1,2,3,4,5], 2))
+
+module.exports = chunk;
